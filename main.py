74f740ce4869e9087685ab1e0ef2a96dea508ab6
louigi000 <louigi@naver.com>
2025-02-09 18:19:38 +0900

    Add Text edit te1 and fix btn1 handler function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # ch 4.2.3. main.py
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox) 
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) 
 from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
 
 class Calculator(QWidget): # QWidget 크래스를 상속받아서 클래스를 정의
@@ -11,13 +11,20 @@ class Calculator(QWidget): # QWidget 크래스를 상속받아서 클래스를 �
         self.initUI()# 나머지 초기화는 initUI 함수에 정의
     
     def initUI(self):
+        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
+        self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정정
+        
         self.btn1=QPushButton('Message', self) # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
         
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
-        vbox.addStretch(1) # 빈 공간
-        vbox.addWidget(self.btn1) # 버튼 위치
-        vbox.addStretch(1) # 빈 공간
+        vbox.addWidget(self.te1) # 수직 레이아웃에 텍스트 에디트 위젯 추가
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        
+        # vbox.addStretch(1) # 빈 공간
+        # vbox.addWidget(self.btn1) # 버튼 위치
+        # vbox.addStretch(1) # 빈 공간
         
         self.setLayout(vbox) # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정정
         
@@ -27,7 +34,8 @@ class Calculator(QWidget): # QWidget 크래스를 상속받아서 클래스를 �
         self.show() # 윈도 화면이 표시되도록 호출
         
     def activateMessage(self): # 버튼을 클릭할 때 동작하는 함수 : 메시지 박스 출력
-        QMessageBox.information(self, "information", "Button clicked")
+        # QMessageBox.information(self, "information", "Button clicked")
+        self.te1.appendPlainText("Button clicked!")
         
     
 if __name__=='__main__': # pyqt는 애플리케이션 당 1개의 QApplication이 필요함
