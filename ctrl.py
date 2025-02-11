cfd2a6c304598de3dfae9ac78c9b01749c6dfb8a
rosaria000 <rosaria000@naver.com>
2025-02-12 01:00:37 +0900

    Modify ctrl.py to connect sum operation to display

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,4 @@
-# ch 6.2.2 ctrl.py
+# ch 6.6.7 ctrl.py
 class Control:
     
     def __init__(self, view):
@@ -6,15 +6,19 @@ class Control:
         self.connectSignals()
         
     def calculate(self): # calculate 메서드 추가. 내용은 추후에 작성
-        pass
+        num1 = float(self.view.le1.text()) # 첫 번재 라인 에디트에 입력된 숫자를 읽어옴
+        num2 = float(self.view.le2.text()) # 두 번재 라인 에디트에 입력된 숫자를 읽어옴
+        operator = self.view.cb.currentText() # 콤보 박스에 선댁된 연산자 확인
         
-    def connectSignals(self):
+        if operator == '+': # 연산자가 +이면 덧셈 결과를 문자열로 리턴
+            return f'{num1} + {num2} = {self.sum(num1, num2)}'
+        else:
+            return 'Calculation Error' # 연산자가 +가 아니면 에러 메시지를 리턴
+        
+    def connectSignals(self): # btn1을 클릭하면 calculate 결과가 화면에 표시되도록 수정정
         # self.view.btn1.clicked.connect(self.view.activateMessage)
-        self.view.btn1.clicked.connect(self.calculate) # 버튼1 연결을 변경
+        self.view.btn1.clicked.connect(lambda:self.view.setDisplay(self.calculate())) # 버튼1 연결을 변경
         self.view.btn2.clicked.connect(self.view.clearMessage)
     
-    def sum(self, a, b): # 예외 처리 기능 추가 
-        try:
-            return str(a+b)
-        except:
-            return 'Calculation Error'
+    def sum(self, a, b): # 예외 처리 제거: 향후 calculate 하무에서 처리하도록 구현 예정정
+            return a+b
