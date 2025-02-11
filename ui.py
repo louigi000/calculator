ad4e43761db94c1148f4380333e0768c234168f6
louigi000 <louigi@naver.com>
2025-02-11 22:33:47 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -1,6 +1,9 @@
-# ch 5.2.1 ui.py
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
+# ch 6.2.1 ui.py
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
+                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, 
+                             QComboBox)
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore # 모듈 추가가
 
 class View(QWidget):
     
@@ -15,6 +18,20 @@ class View(QWidget):
         self.btn1 = QPushButton('Message', self)
         self.btn2 = QPushButton('Clear', self)
         
+        self.le1=QLineEdit('0',self) # 라인 에디트1 추가
+        self.le1.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트1 문자열 배치 설정
+        
+        self.le2=QLineEdit('0',self) # 라인 에디트2 추가
+        self.le2.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트2 문자열 배치 설정
+        
+        self.cb = QComboBox(self) # 콤보 박스 추가
+        self.cb.addItems(['+', '-', '*', '/']) # 콤보 박스 항목 추가(연산자로 사용)
+        
+        hbox_formular = QHBoxLayout() # 새로 정의한 위벳을 QHBoxLayout에 위치
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+        
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
@@ -22,6 +39,7 @@ class View(QWidget):
         
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular) # hbox_formular 배치치
         vbox.addLayout(hbox)
         vbox.addStretch(1)
         
