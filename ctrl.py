ddd2a159411fe08dc239753cb7692d6dfbf0a9a0
louigi000 <louigi@naver.com>
2025-02-12 16:17:57 +0900

    Modify div function using exception

--- ctrl.py
+++ ctrl.py
@@ -29,11 +29,13 @@ class Control:
     def mul(self, a, b): # 곱셈 함수 추가
         return a*b
     
-    def div(self, a, b): # div 함수 수정
-        if (b == 0):
-            return 0
-        else:
-            return a/b
-    
+    def div(self, a, b): # 예외 처리를 사용하도록 수정
+        try:
+            if (b == 0):
+                raise Exception("Divisor Error")
+        except Exception as e:
+            return e
+        return a/b
+
     def pow(self, a, b): # 거듭제곱 함수 추가
         return pow(a, b)
